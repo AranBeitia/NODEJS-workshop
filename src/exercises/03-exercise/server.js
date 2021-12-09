fb211c68f06a8629f0326e0f80b977440c7a421f
AranBeitia <arantza.beitia@gmail.com>
2021-12-09 18:06:55 +0100

    3 exercise

--- src/exercises/03-exercise/server.js
+++ src/exercises/03-exercise/server.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const userData = require("./user-data");
+const express = require('express')
+const userData = require('./user-data')
 
 /**
  * 1. Define the app and assign it to the `app` const
  */
-const app = null;
+const app = express()
 
 /**
  * 2. Add the `express.json()` middleware to use it for all the requests
  */
-// app.use();
+app.use(express.json())
 
 /**
  * 3. Define a GET request handler for the `/user-data` endpoint
@@ -17,6 +17,11 @@ const app = null;
  *    and an object with a property named `data` and as values
  *    the contents of the `userData` object required above
  */
+app.get('/user-data', (req, res) => {
+	res.status(200).send({
+		data: { ...userData },
+	})
+})
 
 /**
  * 4. Define a DELETE request handler for the `/users/:userId` endpoint
@@ -29,6 +34,10 @@ const app = null;
  * 4.3 A response with the following shape:
  *     { `data`: `Deleted ${userId}` }
  */
+app.delete('/users/:userId', (req, res) => {
+	const { userId } = req.params
+	res.status(200).send({ data: `Deleted ${userId}` })
+})
 
 /**
  * 5. Define a PATCH request handler for the `/users/:userId` endpoint
@@ -48,4 +57,15 @@ const app = null;
  *     }
  */
 
-module.exports = app;
+app.patch('/users/:userId', (req, res) => {
+	const { userId } = req.params
+	const { firstName, lastName } = req.body
+	res.status(200).send({
+		data: {
+			id: userId,
+			firstName,
+			lastName,
+		},
+	})
+})
+module.exports = app
